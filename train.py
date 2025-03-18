7f83ef0f69850165ebc7935290d9568dc19fe2ee
Schimmelpfennig, Hendric <heschimm@students.uni-mainz.de>
2025-03-18 01:52:31 +0100

    Add VAE configuration, implement model checkpointing, and introduce learning rate finder

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import lightning
+from lightning.pytorch.callbacks import ModelCheckpoint, LearningRateFinder
 import yaml
 
 from data.era5 import ERA5DataModule
@@ -16,9 +17,21 @@ def main():
 
     autoencoder = Autoencoder(config)
 
+    checkpoint_callback = ModelCheckpoint(
+        dirpath="checkpoints/",
+        filename=config["config"]["general"]["name"] + "-{step}",
+        every_n_train_steps=100,
+
+    )
+
+    lr_finder = LearningRateFinder(mode="linear")
+
     trainer = lightning.Trainer(
         max_epochs=config["config"]["training"]["epochs"],
         precision="16-mixed",
+        callbacks=[checkpoint_callback, lr_finder],
+        accumulate_grad_batches=4,
+
     )
 
     trainer.fit(autoencoder, datamodule=data)
